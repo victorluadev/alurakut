329625fda1398542c1e23a105b91e04ffd5e5786
victorluadev <streamyodin@gmail.com>
2021-07-13 23:38:17 -0300

    finalizando ajustes do dia 1

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,61 @@
+import { useEffect, useState } from 'react';
 import Box from "../src/components/Box";
 import MainGrid from "../src/components/MainGrid";
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(props){
   return (
-    <Box>
-      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}/>
+    <Box as="aside">
+      <img src={`https://github.com/${props.githubUser}.png`} style={{borderRadius: '8px'}}/>
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
   const githubUser = 'victorluadev';
-  const pessoasFavoritas = [
-    'juunegreiros',
-    'omariosouto',
-    'peas',
-    'rafaballerini',
-    'marcobrunodev',
-    'felipefialho'
-  ]
+
+  const [userData, setUserData] = useState({});
+  const [followers, setFollowers] = useState([]);
+  const [following, setFollowing] = useState([]);
+
+  useEffect(() => {
+    async function handleLoadFollowers() {
+      await fetch(`https://api.github.com/users/${githubUser}/followers`)
+      .then(user => user.json())
+      .then(user => setFollowers(user))
+      .catch(err => console.log(err));
+
+      await fetch(`https://api.github.com/users/${githubUser}/following`)
+      .then(user => user.json())
+      .then(user => setFollowing(user))
+      .catch(err => console.log(err));
+    }
+
+    async function handleUserData() {
+      await fetch(`https://api.github.com/users/${githubUser}`)
+      .then(user => user.json())
+      .then(data => setUserData(data))
+      .catch(err => console.log(err));
+    }
+
+    handleLoadFollowers();
+    handleUserData();
+  }, [])
 
   return (
     <>
-      <AlurakutMenu githubUser={githubUser} />
+      <AlurakutMenu githubUser={userData.login} />
       <MainGrid>
         <div className="profileArea" style={{gridArea: 'profileArea'}}>
           <ProfileSidebar githubUser={githubUser}/>
@@ -32,28 +63,50 @@ export default function Home() {
         <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
           <Box>
             <h1 className="title">
-              Bem vindo(a) 
+              Bem vindo(a), {userData.name}
             </h1>
 
-            <OrkutNostalgicIconSet />
+            <OrkutNostalgicIconSet recados="7" fotos="2" videos="7" fas="9" mensagens="4" confiavel="3" legal="3" sexy="2"/>
           </Box>
         </div>
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
         <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
-              Pessoas da comunidade ({pessoasFavoritas.length})
+              Seguidores ({followers.length})
             </h2>
 
             <ul>
-              {pessoasFavoritas.map((itemAtual) => {
-                return (
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
-                      <img src={`https://github.com/${itemAtual}.png`} />
-                      <span>{itemAtual}</span>
-                    </a>
-                  </li>
-                )
+              {followers.map((itemAtual, index) => {
+                if(index <= 5){
+                  return (
+                    <li>
+                      <a href={`${itemAtual.html_url}`} key={index}>
+                        <img src={`https://github.com/${itemAtual.login}.png`} />
+                        <span>{itemAtual.login}</span>
+                      </a>
+                    </li>
+                  )
+                }
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Seguindo ({following.length})
+            </h2>
+
+            <ul>
+              {following.map((itemAtual, index) => {
+                if(index <= 5){
+                  return (
+                    <li>
+                      <a href={`${itemAtual.html_url}`} key={index}>
+                        <img src={`https://github.com/${itemAtual.login}.png`} />
+                        <span>{itemAtual.login}</span>
+                      </a>
+                    </li>
+                  )
+                }
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
